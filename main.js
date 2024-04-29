fa053b8607b5c56569c6dadb0ba05c1ceaeb5f5e
galihbulann <168471226+galihbulann@users.noreply.github.com>
2024-04-30 01:17:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-window.onload = () => {
-    document.body.classList.remove("container");
-    const audioElement = document.querySelector("audio");
-    if(audioElement) {
-        audioElement.play();
-    }
-};
+const audioTest = new Audio("Sound.mp3");
+audioTest.play().then(() => {
+  console.log("Audio played successfully.");
+}).catch(error => {
+  console.error("Error playing audio:", error);
+});
